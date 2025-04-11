4965d624318a12d50711a9995f9bf32db8772d1c
Iury Silva <iurygon.silva@gmail.com>
2025-04-10 22:56:31 -0300

    Início Projeto

---
+++ script.js
@@ -0,0 +1,21 @@
+const listaComprasCozinha = ['arroz','feijão','café','açúcar','sal','macarrão','azeitona','óleo','maionese','maisena','óleo de coco','leite condensado','leite','creme de leite','coco ralado','molho shoyo','papel toalha','tempero','bolacha','alho','cebola','frutas','legumes','verduras']
+const listaComprasLimpeza = ['quiboa','detergente','bombril','esponja louça','sabão em pó','sabão em barra','saco de lixo']
+const listaComprasBanheiro = ['papel higiênico','sabonete','pasta de dente','escova de dente','fio dental','enxaguante bucal','cotonete','algodão','acetona','shampoo','condicionador']
+
+const listaComprasCozinhaOrdenada = listaComprasCozinha.sort()
+const listaComprasLimpezaOrdernada = listaComprasLimpeza.sort()
+const listaComprasBanheiroOrdenada = listaComprasBanheiro.sort()
+
+window.onload =  () => {
+    adicionaItems()
+}
+
+function adicionaItems(){
+    listaComprasCozinhaOrdenada.forEach((item) => {
+
+    })
+}
+
+function criaElementoLista(textoElemento){
+    
+}

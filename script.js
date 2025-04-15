30006398fe34fb45234b5e5f3e9f872471175acb
Iury Silva <iurygon.silva@gmail.com>
2025-04-14 23:14:27 -0300

    Finalização da criação da lista

--- script.js
+++ script.js
@@ -6,16 +6,42 @@ const listaComprasCozinhaOrdenada = listaComprasCozinha.sort()
 const listaComprasLimpezaOrdernada = listaComprasLimpeza.sort()
 const listaComprasBanheiroOrdenada = listaComprasBanheiro.sort()
 
+const ulCozinha = document.querySelector('.listaConteudoCozinha')
+const ulLimpeza = document.querySelector('.listaConteudoLimpeza')
+const ulBanheiro = document.querySelector('.listaConteudoBanheiro')
+ulCozinha.innerHTML = ''
+ulLimpeza.innerHTML = ''
+ulBanheiro.innerHTML = ''
+
 window.onload =  () => {
     adicionaItems()
 }
 
 function adicionaItems(){
     listaComprasCozinhaOrdenada.forEach((item) => {
-
+        criaElementoLista(ulCozinha,item)
+    })
+    listaComprasLimpezaOrdernada.forEach((item) => {
+        criaElementoLista(ulLimpeza,item)
+    })
+    listaComprasBanheiroOrdenada.forEach((item) => {
+        criaElementoLista(ulBanheiro,item)
     })
 }
 
-function criaElementoLista(textoElemento){
-    
+function criaElementoLista(lista,textoElemento){
+    const li = document.createElement('li')
+    li.classList.add('itemListaConteudo')
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.classList.add('checkMark')
+
+    const label = document.createElement('label')
+    label.classList.add('itemDesc')
+    label.textContent = `${textoElemento}`
+
+    li.appendChild(checkbox)
+    li.appendChild(label)
+    lista.appendChild(li)
 }
